ceedf50ae6353e870e2b90ab5df8ddcbd6cb3ba4
toan0412 <truongtoan00189@gmail.com>
2024-03-27 22:50:24 +0700

    prettierrc setting

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
 
-
 function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  )
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    );
 }
 
 export default App;
